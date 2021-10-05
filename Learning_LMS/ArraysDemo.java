8ae3760732c6765c7a7d333cffffefe253f2b739
phantombmt <nguyen.tran.vovinam@gmail.com>
2021-10-05 14:09:18 +0900

    Update

--- Learning_LMS/ArraysDemo.java
+++ Learning_LMS/ArraysDemo.java
@@ -20,10 +20,27 @@ public class ArraysDemo {
 		// arr[7] = 70;
 
 		System.out.println("Elements of array : ");
+		
+		//Print array starting from the first element
+		
 		for (int i = 0; i < arr.length; i++) {
 
 			System.out.println(arr[i]);
 
+		}
+		
+		//Print array starting from the last element
+		for (int i = arr.length -1 ; i >=0 ; i--) {
+			
+			System.out.println(arr[i]);
+			
+		}
+		
+		//Print all the element in arr if exists , but can't print in the reverse direction, just only starting from arr[0]
+		
+		for (int element : arr) {
+			
+			System.out.println(element);
 		}
 	}
 
---
+++ Learning_LMS/If_Else_Demo.java
@@ -0,0 +1,18 @@
+
+public class If_Else_Demo {
+
+	public static void main(String[] args) {
+
+		int x = 10, y = 10;
+
+		if (x > y) {
+
+			System.out.println("X is greater");
+		} else if (x == y) {
+			System.out.println("Both numbers are equal");
+		} else {
+			System.out.println("Y is greater");
+		}
+	}
+
+}
